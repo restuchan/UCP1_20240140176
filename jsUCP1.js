d7fb59de7e64c9924b1ca47d6f728ee42e46ba94
restuchan <restu.chan188@gmail.com>
2026-04-23 09:43:19 +0700

    first commit

---
+++ jsUCP1.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video() {
+    alert("Anda mengklik video profil KUI");
+});
+
+audio.addEventListener("click", function audio() {
+    alert("Anda mengklik jingle KUI");
+});
+
+gambar.addEventListener("click", function gambar() {
+    alert("Anda mengklik gambar produk KUI");
+});
